86b5983ceacd723f6d93b37c35aa0091c8b3de44
Caroline Moers <Caroline_Moers@gap.com>
2016-10-12 17:00:06 -0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class Application {
+
+    public static void main(String[] args) {
+        port(9090);
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
